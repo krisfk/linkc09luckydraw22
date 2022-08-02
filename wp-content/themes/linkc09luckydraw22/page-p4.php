0168cb8e459c432b4f247f92ab9b4b8660d80110
kaiyu li <krisfk@gmail.com>
2022-08-03 07:10:58 +0800

    .

--- wp-content/themes/linkc09luckydraw22/page-p4.php
+++ wp-content/themes/linkc09luckydraw22/page-p4.php
@@ -52,7 +52,7 @@ get_header(); ?>
                 </tr>
             </table>
 
-            <div class="text-center orange" style="font-size:40px">
+            <div class="text-center orange mt-3" style="font-size:40px">
                 會員號碼：12345678 <br>
                 你已自動晋級「終極大抽獎」！<br>
                 得獎結果將於2022年10月25日於領展網站公佈，敬請留意
