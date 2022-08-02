e13871c834f1a0368abe0ad05504dfbb6b71ef0e
kaiyu li <krisfk@gmail.com>
2022-08-03 07:31:37 +0800

    .

--- wp-content/themes/linkc09luckydraw22/header.php
+++ wp-content/themes/linkc09luckydraw22/header.php
@@ -39,3 +39,4 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
+                    <div>place</div>
