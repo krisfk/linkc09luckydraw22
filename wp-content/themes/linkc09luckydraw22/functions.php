7ac2a6bab50c6be6b7eabbbb3fe0a1d3887597ed
kaiyu li <krisfk@gmail.com>
2022-08-04 02:40:57 +0800

    .

--- wp-content/themes/linkc09luckydraw22/functions.php
+++ wp-content/themes/linkc09luckydraw22/functions.php
@@ -675,7 +675,7 @@ function add_member_reward_record($request)
 	$member_tel = $request['member_tel'];
 	$prize_post_id = $request['prize_post_id'];
 	$reward_datetime=date("Y-m-d H:i:s");
-	$reward_place = $request['loc'];
+	$reward_place = $request['reward_place'];
 
 
 	$post_id = wp_insert_post(array (
