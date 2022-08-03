f998fdc7667f44a9ae4d90eff3329adce7b734de
kaiyu li <krisfk@gmail.com>
2022-08-04 04:46:16 +0800

    .

--- wp-content/themes/linkc09luckydraw22/header.php
+++ wp-content/themes/linkc09luckydraw22/header.php
@@ -49,8 +49,8 @@
 
 
 
-                    if(!$_REQUEST['loc'] && $_REQUEST['loc']!='-1' && ($post_slug !='cs-check' || $post_slug !='cs-check2') )
+                    if(!$_REQUEST['loc'] && $_REQUEST['loc']!='-1' && ($post_slug !='cs-check' && $post_slug !='cs-check2') )
                     {
-                    // wp_redirect(get_site_url().'/error?loc=-1');
+                    wp_redirect(get_site_url().'/error?loc=-1');
                     }
                     ?>
