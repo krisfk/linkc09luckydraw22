ac54d88b0c22b4dfc5305c866da12f5e8ed3fc9b
kaiyu li <krisfk@gmail.com>
2022-08-04 22:08:08 +0800

    .

--- wp-content/themes/linkc09luckydraw22/header.php
+++ wp-content/themes/linkc09luckydraw22/header.php
@@ -17,9 +17,9 @@
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> -->
-    <meta name="viewport" content="width=2000, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <!-- <meta name="viewport" content="width=2000, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> -->
     <?php wp_head(); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
