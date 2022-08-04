70cc1b1847b02a19412b19e2daa1a11ec7e04a4c
kaiyu li <krisfk@gmail.com>
2022-08-05 00:35:20 +0800

    .

--- wp-content/themes/linkc09luckydraw22/page-p4.php
+++ wp-content/themes/linkc09luckydraw22/page-p4.php
@@ -134,8 +134,8 @@ if(!get_field('prize_name',$bingo_prize_post_id))
                     <td class="pe-4"><input class="form-control page45-form-input" id="staff-input" type="password"
                             placeholder="由工作人員輸入密碼">
                         <div class="error" style="    position: absolute;
-    top: 10px;
-    right: -170px;
+   top: -7px;
+    right: -142px;
     z-index: 100;">密碼不正確</div>
                     </td>
 
