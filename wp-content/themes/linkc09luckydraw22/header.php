fb25e26790e36993814aaded9636bf17bc14eda2
kaiyu li <krisfk@gmail.com>
2022-08-07 04:30:52 +0800

    .

--- wp-content/themes/linkc09luckydraw22/header.php
+++ wp-content/themes/linkc09luckydraw22/header.php
@@ -30,6 +30,11 @@
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 
+
+    <link rel="manifest" href="https://linkc09luckydraw22.com/manifest/manifest-YIT.json">
+    <link rel="prefetch" href="https://linkc09luckydraw22.com/manifest/manifest-YIT.json">
+
+
 </head>
 
 <body <?php body_class(); ?>>
