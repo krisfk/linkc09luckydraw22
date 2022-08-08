20c33ab3b1954b72d2101495cd13267325f38c92
kaiyu li <krisfk@gmail.com>
2022-08-08 14:39:35 +0800

    .

--- wp-content/themes/linkc09luckydraw22/page-list-data.php
+++ wp-content/themes/linkc09luckydraw22/page-list-data.php
@@ -49,7 +49,97 @@ get_header(); ?>
         </li>
     </ul>
 
+
+
+
+    <div class="text-start mt-5">
+        <a href="javascript:void(0);" class="btn btn-info getfile">EXPORT</a>
+    </div>
+
+
+
+
+
 </div>
+
+
+
+
+
+<style type="text/css">
+body {
+    padding: 1rem;
+}
+
+table {
+    width: max-content;
+    border-spacing: 0;
+    border-collapse: collapse;
+
+}
+
+table.excel-table th {
+    padding: 1rem;
+    border: 1px solid #000;
+}
+
+table.excel-table td {
+    padding: 1rem;
+
+    border: 1px solid #000;
+}
+</style>
+
+<script type="text/javascript">
+$(function() {
+    $('.getfile').click(
+        function() {
+            exportTableToCSV.apply(this, [$('#excel-table'), 'tutor-list.csv']);
+        });
+
+})
+
+function exportTableToCSV($table, filename) {
+
+    var $rows = $table.find('tr:has(td)'),
+
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '","',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = '"' + $rows.map(function(i, row) {
+            var $row = $(row),
+                $cols = $row.find('td');
+
+            return $cols.map(function(j, col) {
+                var $col = $(col),
+                    text = $col.text();
+
+                return text.replace('"', '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+        .split(tmpRowDelim).join(rowDelim)
+        .split(tmpColDelim).join(colDelim) + '"',
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+    $(this)
+        .attr({
+            'download': filename,
+            'href': csvData,
+            'target': '_blank'
+        });
+}
+</script>
 
 <?php
 
